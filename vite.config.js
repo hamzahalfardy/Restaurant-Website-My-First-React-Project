493cd92193db35285d51edeb9dee8e6521d5fb1b
hamzahalfardy <160720239+hamzahalfardy@users.noreply.github.com>
2025-08-15 21:01:11 +0700

    add base to vit.cong

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/Restaurant-Website-My-First-React-Project/',
     server: {
     host: '0.0.0.0', // expose to network
     port: 3000       // optional, to keep port fixed
